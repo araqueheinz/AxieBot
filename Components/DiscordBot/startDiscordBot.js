80310bc3a3629a1be488cd623fa62e41ec645ad8
Heinz Araque <araqueheinz@gmail.com>
2021-11-30 12:14:32 -0800

    Refactor

--- Components/DiscordBot/startDiscordBot.js
+++ Components/DiscordBot/startDiscordBot.js
@@ -3,25 +3,25 @@ const setLeaderboard      = require( "./setLeaderboard" );
 const bot                 = require( "../../local/bot.json" );
 const client              = new Client( { intents: [ Intents.FLAGS.GUILDS, "GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES" ] } );
 
+globalScholars = [];
+
 const startBot = async( scholars ) => {
+  // Set the Global Scholar Variable to the received Scholars
+  globalScholars = [ ...scholars ];
   /* Toggle between test_token & token */
-  await client.login( bot.token );
-  /* Toggle between test_channel_ID & channel_ID */
-  const leaderBoardChannel =  await client.channels.fetch( bot.channel_ID );
-  if ( leaderBoardChannel ) {
-    console.log( `\nChannel: ${ leaderBoardChannel.name } found!` );
-    botReady( leaderBoardChannel, scholars );
-  } else {
-    console.log( "ERROR! Could not get the leaderboard channel..." )
-  }
+  client.login( bot.test_token );
 }
 
-const botReady = async ( leaderBoardChannel, scholars ) => {
-  console.log( "Attempting to start the bot..." );
-  await client.on( "ready", () => {
-    console.log( `This bot is now online: ${ client.user.tag }` );
-    // setLeaderboard( leaderBoardChannel, scholars );
-  });
-}
+client.on( "ready", async () => {
+  console.log( `\nDISCORD BOT: "${ client.user.tag }" is now ONLINE!` );
+  /* Toggle between test_channel_ID & channel_ID */
+  const leaderBoardChannel =  await client.channels.fetch( bot.test_channel_ID );
+  if ( leaderBoardChannel ) {
+    console.log( `Channel: ${ leaderBoardChannel.name } found!` );
+    setLeaderboard( leaderBoardChannel, globalScholars );
+  } else {
+    console.log( "ERROR! Could not get the leaderboard channel..." );
+  }
+});
 
 module.exports = startBot;
